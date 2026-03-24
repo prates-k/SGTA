c02315723cbc126e706a04adf6f14e9a0fa2ab9c
prates-k <eng.kauaprates@gmail.com>
2026-03-23 21:22:25 -0300

    feat: adicionar rota listar tarefas abertas

--- backend/tarefas/urls.py
+++ backend/tarefas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listar_tarefas
+from .views import listar_tarefas, listar_tarefas_abertas
 
 urlpatterns = [
-    path('tarefas/', listar_tarefas)
+    path('tarefas/', listar_tarefas),
+    path('tarefas/abertas/', listar_tarefas_abertas),
 ]
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -6,3 +6,7 @@ from .models import Tarefa
 def listar_tarefas(request):
     tarefas = Tarefa.objects.all().values()
     return JsonResponse(list(tarefas), safe=False)
+
+def listar_tarefas_abertas(request):
+    tarefas = Tarefa.objects.filter(status='ABERTA').values()
+    return JsonResponse(list(tarefas), safe=False)
