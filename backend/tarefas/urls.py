8f6d70249402de02eecf209b8fd850374658ed8b
prates-k <eng.kauaprates@gmail.com>
2026-04-06 20:50:45 -0300

    feat: adiciona rotas de busca por id, prioridade, atrasadas e busca por titulo

--- backend/tarefas/urls.py
+++ backend/tarefas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import listar_tarefas, listar_tarefas_abertas
+from .views import listar_tarefas, listar_tarefas_abertas, listar_tarefas_por_prioridade
 
 urlpatterns = [
     path('tarefas/', listar_tarefas),
     path('tarefas/abertas/', listar_tarefas_abertas),
+    path('tarefas/prioridade/<str:prioridade>/', listar_tarefas_por_prioridade),
 ]
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -10,3 +10,46 @@ def listar_tarefas(request):
 def listar_tarefas_abertas(request):
     tarefas = Tarefa.objects.filter(status='ABERTA').values()
     return JsonResponse(list(tarefas), safe=False)
+
+def listar_tarefas_por_prioridade(request, prioridade):
+    tarefas = Tarefa.objects.filter(status=prioridade).values()
+    return JsonResponse(list(tarefas), safe=False)
+    
+
+def buscar_tarefa_por_id(request, id):
+    try:
+        tarefa = Tarefa.objects.get(id=id)
+        return JsonResponse({
+            'id': tarefa.id,
+            'titulo': tarefa.titulo,
+            'status': tarefa.status,
+            'prioridade': tarefa.prioridade
+        })
+    except Tarefa.DoesNotExist:
+        return JsonResponse({'erro': 'Tarefa não encontrada'}, status=404)
+    
+def listar_tarefas_abertas_urgentes(request):
+    tarefas = Tarefa.objects.filter(
+        status='ABERTA',
+        prioridade='URGENTE'
+    ).values()
+
+    return JsonResponse(list(tarefas), safe=False)
+
+from datetime import date
+
+def listar_tarefas_atrasadas(request):
+    hoje = date.today()
+
+    tarefas = Tarefa.objects.filter(
+        data_entrega__lt=hoje
+    ).exclude(status='CONCLUIDA').values()
+
+    return JsonResponse(list(tarefas), safe=False)
+
+def buscar_tarefas_por_titulo(request, palavra):
+    tarefas = Tarefa.objects.filter(
+        titulo__icontains=palavra
+    ).values()
+
+    return JsonResponse(list(tarefas), safe=False)
